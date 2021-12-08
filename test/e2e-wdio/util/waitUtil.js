f1bd471c8009f9beec84695758cae0d032f2c81e
Yuliia Lysenko <yuliia.lysenko@okta.com>
2021-12-08 16:18:55 +0200

    add waiter for ie11

--- test/e2e-wdio/util/waitUtil.js
+++ test/e2e-wdio/util/waitUtil.js
@@ -1,3 +1,8 @@
 export const waitForLoad = async (element) => {
-  await browser.waitUntil(async () => element.then(el => el.isDisplayed()), 5000, 'wait for element to load');
+  if(browser.capabilities.browser.localeCompare('internet explorer')){
+    await new Promise(r => setTimeout(r, 5000));
+    browser.isElementDisplayed(element);
+  } else {
+    await browser.waitUntil(async () => element.then(el => el.isDisplayed()), 5000, 'wait for element to load');
+  }
 };
