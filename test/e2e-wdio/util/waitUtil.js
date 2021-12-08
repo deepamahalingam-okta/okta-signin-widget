16f10c94b63c29d00229d182fc5998bb28975b03
Yuliia Lysenko <yuliia.lysenko@okta.com>
2021-12-08 17:26:42 +0200

    fix waitfor for ie11

--- test/e2e-wdio/util/waitUtil.js
+++ test/e2e-wdio/util/waitUtil.js
@@ -1,10 +1,10 @@
 export const waitForLoad = async (element) => {
     const currentBrowser = browser.capabilities.browserName;
-    if(currentBrowser.localeCompare('Internet Explorer 11') ||
-      currentBrowser.localeCompare('iexplore') ||
-      currentBrowser.localeCompare('internet explorer')){
-    await new Promise(r => setTimeout(r, 5000));
-    browser.isElementDisplayed(element);
+    if( currentBrowser === 'Internet Explorer 11' ||
+      currentBrowser ==='iexplore' ||
+      currentBrowser ==='internet explorer'){
+      await new Promise(r => setTimeout(r, 10_000));
+      browser.isElementDisplayed(element);
   } else {
     await browser.waitUntil(async () => element.then(el => el.isDisplayed()), 5000, 'wait for element to load');
   }
